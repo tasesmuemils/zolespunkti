0f567a15f59b7c4196c360bc28d473afaca0c1c9
tasesmuemils <emils.bisenieks93@gmail.com>
2024-07-31 17:09:57 +0300

    mw update

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,16 @@
 import { updateSession } from '@/utils/superbase/middleware';
 
 export async function middleware(request) {
-  // Check if the request is for a CSS file
-  // Check if the request is for static assets
-  if (
-    request.nextUrl.pathname.startsWith('/_next/') ||
-    request.nextUrl.pathname.match(
-      /\.(js|css|svg|png|jpg|jpeg|gif|webp|ico|json)$/
-    )
-  ) {
-    return NextResponse.next();
-  }
+  // // Check if the request is for a CSS file
+  // // Check if the request is for static assets
+  // if (
+  //   request.nextUrl.pathname.startsWith('/_next/') ||
+  //   request.nextUrl.pathname.match(
+  //     /\.(js|css|svg|png|jpg|jpeg|gif|webp|ico|json)$/
+  //   )
+  // ) {
+  //   return NextResponse.next();
+  // }
   return await updateSession(request);
 }
 
