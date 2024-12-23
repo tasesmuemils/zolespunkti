c345421038ca6ebcf1997de99b9c01c262ded84c
tasesmuemils <emils.bisenieks93@gmail.com>
2024-12-23 14:33:08 +0200

    New landing page with hero cards and icons

--- src/components/layout/header-main.jsx
+++ src/components/layout/header-main.jsx
@@ -1,25 +1,51 @@
 import { ModeToggle } from '@/components/component/mode-toogle';
 import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu';
 
 export default function Navbar() {
   return (
-    <div className='supports-backdrop-blur:bg-background/60 fixed left-0 border-b right-0 top-0 z-20 bg-background/95 backdrop-blur'>
-      <nav className='flex h-14 items-center justify-between px-4'>
-        <div className='block'>
-          <Link href={'/'}>
-            {/* <Image src={FullLogo} alt='full_logo' width={147} /> */}
-            <p>Zoles Punkti</p>
-          </Link>
-        </div>
-        {/* <div className={cn('block lg:!hidden')}>
-          <MobileSidebar />
-        </div> */}
+    <header className='sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background'>
+      <NavigationMenu className='mx-auto'>
+        <NavigationMenuList className='container h-14 px-4 w-screen flex justify-between '>
+          <NavigationMenuItem className='font-bold flex'>
+            <a
+              rel='noreferrer noopener'
+              href='/'
+              className='ml-2 font-bold text-xl flex'
+            >
+              Zoles Punkti
+            </a>
+          </NavigationMenuItem>
 
-        <div className='flex items-center gap-4'>
-          {/* <UserNav /> */}
-          <ModeToggle />
-        </div>
-      </nav>
-    </div>
+          <div className='flex gap-2'>
+            <ModeToggle />
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
   );
+  // return (
+  //   <div className='sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background'>
+  //     <nav className='flex h-14 items-center justify-between px-4'>
+  //       <div className='block'>
+  //         <Link href={'/'}>
+  //           {/* <Image src={FullLogo} alt='full_logo' width={147} /> */}
+  //           <p>Zoles Punkti</p>
+  //         </Link>
+  //       </div>
+  //       {/* <div className={cn('block lg:!hidden')}>
+  //         <MobileSidebar />
+  //       </div> */}
+
+  //       <div className='flex items-center gap-4'>
+  //         {/* <UserNav /> */}
+  //         <ModeToggle />
+  //       </div>
+  //     </nav>
+  //   </div>
+  // );
 }
