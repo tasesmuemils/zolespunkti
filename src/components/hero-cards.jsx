0885d4f33cf04966cb4a1245b8b6c6a35bfbb6e2
tasesmuemils <emils.bisenieks93@gmail.com>
2025-01-02 00:56:11 +0200

    Extra deploy

--- src/components/hero-cards.jsx
+++ src/components/hero-cards.jsx
@@ -153,7 +153,7 @@ export const HeroCards = () => {
         </CardHeader>
 
         <CardContent>
-          <Button className='w-full'>Start Free Trial</Button>
+          <Button className='w-full'>Start Free Trial</Button>zzzzz
         </CardContent> */}
 
         <hr className='w-4/5 m-auto mb-4' />
